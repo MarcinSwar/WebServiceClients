5eca385327aa881112845d72ae5d3cf282802fa4
MarcinSwar <C1nx@github>
2019-04-09 00:49:57 +0200

    completed backend

---
+++ WebServiceClients/src/zad1/Main.java
@@ -0,0 +1,42 @@
+
+package zad1;
+
+import java.util.Currency;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Set;
+
+import org.json.*;
+public class Main {
+  public static void main(String[] args) {
+    Service s = new Service("Spain");
+    String weatherJson = s.getWeather("Warsaw");
+    Double rate1 = s.getRateFor("USD");
+    Double rate2 = s.getNBPRate();
+    // ...
+    // część uruchamiająca GUI
+    
+    
+    
+    System.out.println(weatherJson);
+    System.out.println(rate1);
+    System.out.println(rate2);
+//    JSONObject jsonWeather = new JSONObject(weatherJson);
+//    String countryCode = jsonWeather.getJSONObject("sys").getString("country");
+//    System.out.println(countryCode);
+//    Locale locale = new Locale(countryCode);
+//    System.out.println(locale);
+////    Currency currentCurrency = Currency.getInstance(locale);
+////    System.out.println(locale.getDisplayName());
+////    System.out.println(currentCurrency.getDisplayName());
+//    Currency currency = Currency.getInstance(Locale.POLAND);
+//    System.out.println(currency);
+////    Set keys = jsonWeather.keySet();
+//    Iterator iter = keys.iterator();
+//    while(iter.hasNext()) {
+//    	System.out.println(iter.next());
+//    }
+    
+    //Set keys = weatherJson.key
+  }
+}
---
+++ WebServiceClients/src/zad1/Service.java
@@ -0,0 +1,139 @@
+package zad1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Currency;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.json.JSONObject;
+
+public class Service {
+	
+	String country;
+	//String city;
+	
+	
+	Map<String, String> currencySymbols = new HashMap<String, String>() {
+		{
+			put("Poland", "PLN");
+			put("USA", "USD");
+			put("Australia", "AUD");
+			put("Canada", "CAD");
+			put("Germany", "EUR");
+			put("France", "EUR");
+			put("Spain", "EUR");
+			put("Russia", "RUB");
+			put("Sweden", "SEK");
+		}
+	};
+	
+	
+	
+
+	public Service(String string) {
+		country = string;
+		//getCurrencyOfTheCountry(country);
+	}
+	
+//	private static String getCurrencyOfTheCountry(String country) {
+//		
+//		Locale locale = Locale.getDefault();
+//				//new Locale(country);
+//		System.out.println(locale);
+//		Currency currency = Currency.getInstance(locale);
+//		System.out.println(currency);
+//		return country;
+//	}
+
+	public String getWeather(String city) {
+		final String APPID = "d8764cfd9e372e9a6df27281b033684e";
+		String preparedURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city
+			+ ",country=country&mode=json&appid="+APPID;
+		String inputLine = "";
+		String weatherJSON = "";
+		
+		try {
+			URL openWeather = new URL(preparedURL);
+			URLConnection urlconnection = openWeather.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(urlconnection.getInputStream()));
+			while((inputLine = in.readLine()) != null)
+				weatherJSON = inputLine;
+			in.close();
+				
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return weatherJSON;
+	}
+
+	public Double getRateFor(String currency) {
+		String preparedURL = "https://api.exchangeratesapi.io/latest?base="+currencySymbols.get(country);
+		String inputLine="";
+		String currencyRateJSON="";
+		try {
+			URL exchangeRatesApi = new URL(preparedURL);
+			URLConnection urlconnection = exchangeRatesApi.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(urlconnection.getInputStream()));
+			while((inputLine = in.readLine()) != null)
+				currencyRateJSON = inputLine;
+			in.close();
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		JSONObject jsonCurrency = new JSONObject(currencyRateJSON);
+		Double currencyRate = jsonCurrency.getJSONObject("rates").getDouble(currency);
+		
+		return currencyRate;
+	}
+
+	public Double getNBPRate() {
+		
+		String preparedURL = "http://api.nbp.pl/api/exchangerates/rates/a/" + currencySymbols.get(country) + "/";
+		String inputLine = "";
+		String nbpRateJson="";
+		Double nbpRate=null;
+		
+		if (currencySymbols.get(country).equals("PLN"))
+		{
+			return 1.0;
+		} else
+		{
+			
+			try {
+				URL nbpApi = new URL(preparedURL);
+				URLConnection urlconnection = nbpApi.openConnection();
+				BufferedReader in = new BufferedReader(new InputStreamReader(urlconnection.getInputStream()));
+				while((inputLine = in.readLine()) != null)
+					nbpRateJson = inputLine;
+				in.close();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			Pattern pattern = Pattern.compile("[\\d]+[.][\\d]+");
+			Matcher matcher = pattern.matcher(nbpRateJson); 
+
+			while (matcher.find())
+				nbpRate = Double.parseDouble(matcher.group()); 
+			return nbpRate;
+			
+//			JSONObject jsonNbpRate = new JSONObject(nbpRateJson);
+//			JSONObject jsonRates = jsonNbpRate.getJSONObject("rates");
+//			//Double nbpRate = jsonNbpRate.getJSONObject("rates").getDouble("mid");
+			
+		}
+	}
+}  
